24f1066867fcaa576655c25930e2d0a4d1ea7d57
William Yang <mscame@gmail.com>
2021-03-09 14:00:06 +0100

    Add License header for C src files.

--- c_src/quicer_stream.h
+++ c_src/quicer_stream.h
@@ -1,3 +1,19 @@
+/*--------------------------------------------------------------------
+Copyright (c) 2021 EMQ Technologies Co., Ltd. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-------------------------------------------------------------------*/
+
 #ifndef __QUICER_STREAM_H_
 #define __QUICER_STREAM_H_
 
@@ -18,8 +34,8 @@ ERL_NIF_TERM close_stream1(ErlNifEnv *env, int argc,
                            const ERL_NIF_TERM argv[]);
 
 _IRQL_requires_max_(DISPATCH_LEVEL)
-_Function_class_(QUIC_STREAM_CALLBACK) QUIC_STATUS QUIC_API
-ClientStreamCallback(_In_ HQUIC Stream, _In_opt_ void *Context,
+    _Function_class_(QUIC_STREAM_CALLBACK) QUIC_STATUS QUIC_API
+    ClientStreamCallback(_In_ HQUIC Stream, _In_opt_ void *Context,
                          _Inout_ QUIC_STREAM_EVENT *Event);
 
 #endif // __QUICER_STREAM_H_
