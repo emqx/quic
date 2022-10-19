f055d76bf084fcb8aba2db1c764bd08d7f8cca85
Claes Wikstrom <klacke@gmail.com>
2022-10-19 15:08:53 +0200

    Update

--- test/example/qt.erl
+++ test/example/qt.erl
@@ -21,6 +21,10 @@
 %% ok
 %% -->
 
+%% Also interesting is to experiment with various variants of CTL-Z and
+%% CTL-C on both the server and the client.
+
+
 
 %% The server
 
@@ -72,7 +76,7 @@ acceptor(Top, L) ->
 
 
 accept_stream_loop(Conn) ->
-    case accept_stream(Conn, [{active, false}], infinity) of
+    case quicer:accept_stream(Conn, [{active, false}], infinity) of
         {ok, Stm} ->
             io:format("accept stream -> ~p\n",[Stm]),
 
@@ -81,12 +85,9 @@ accept_stream_loop(Conn) ->
                   fun() -> stream_owner(Self, Stm) end),
             ok = quicer:controlling_process(Stm, H),
 
-
             receive
                 {H, continue} ->
-                    accept_stream_loop(Conn);
-                {H, turn} ->
-                    turn(Conn)
+                    accept_stream_loop(Conn)
             end;
         {error, connection_closed} ->
             io:format("Connection closed \n",[]),
@@ -97,65 +98,37 @@ accept_stream_loop(Conn) ->
     end.
 
 stream_owner(Top, Stream) ->
+    Top ! {self(), continue},
     case server_negotiate(Stream) of
         {ok, CNo, SNo} ->
             io:format("Enter recv_ping for ~p:~p\n",[CNo, SNo]),
 
             ok = quicer:setopt(Stream, active, 20),
-            case recv_ping(Stream, 1, CNo, SNo) of
-                turn ->
-                    Top ! {self(),turn},
-                    io:format("Turning connecton \n",[]);
-
-                _ ->
-                    Top ! {self(), continue},
-                    ok
-            end,
+            recv_ping(Stream, 1, CNo, SNo),
             io:format("Leaving stream_Owner \n",[]);
         Err ->
             io:format("server negotiate ~p~n", [Err])
     end.
 
 
-turn(Conn) ->
-    case quicer:start_stream(Conn, [{active, 30}]) of
-        {ok, Stm} ->
-            case client_negotiate(Stm, 1,
-                                  1) of
-                ok ->
-                    ok = quicer:setopt(Stm, active, 30),
-                    io:format("Stream # ~p~n",[1]),
-                    Ret = send_ping(1, Stm),
-                    io:format("Turned ping = ~p~n", [Ret]),
-                    quicer:close_stream(Stm),
-                    quicer:close_connection(Conn);
-                Err ->
-                    io:format("Faila ~p~n", [Err])
-            end;
-        Err ->
-            io:format("Fail to start stream ~p~n", [Err])
-    end.
 
 recv_ping(S, N, CNo, SNo) ->
     receive
         {quic_passive, S} ->
             ok = quicer:setopt(S, active, 20),
             recv_ping(S, N, CNo, SNo);
-        {quic, <<"turn">>, S,_,_, _} ->
-            quicer:close_stream(S),
-            turn;
-        {quic, <<"ping">>, S,_,_, _} ->
+        {quic, <<"ping">>, S,_} ->
             io:format("Got ping ~p from ~p:~p\n",[N, CNo, SNo]),
             {ok, 4} = quicer:send(S, <<"pong">>),
             recv_ping(S, N+1, CNo, SNo);
-        {quic, <<"die">>, S,_,_, _} ->
+        {quic, <<"die">>, S,_} ->
             io:format("Got die from ~p:~p\n",[CNo, SNo]),
             die;
         {quic, peer_send_shutdown, S} ->
             io:format("Got peer shutdown from ~p:~p\n",[CNo, SNo]),
             quicer:close_stream(S),
             closed;
-        {quic, closed, S, _X} ->
+        {quic, stream_closed, S, _X} ->
             io:format("Got close stream from ~p:~p\n",[CNo, SNo]),
             quicer:close_stream(S),
             closed
@@ -168,17 +141,14 @@ send_ping(N, S) ->
     rec_pong(N, S).
 rec_pong(N, S) ->
     receive
-        {quic, <<"pong">>, S,_,_, _} ->
+        {quic, <<"pong">>, S,_} ->
             io:format("Got pong ~p\n",[N]),
             send_ping(N-1, S);
         {quic_passive, S} ->
             ok = quicer:setopt(S, active, 30),
             rec_pong(N, S);
-        {quic, S, closed} ->
-            closed;
-        {quic, S, closed, _} ->
-            closed2
-
+        {quic, stream_closed, S, _} ->
+            closed
     end.
 
 
@@ -206,14 +176,15 @@ client(Conns, Streams, CNo, SNo) ->
                     client(Conns, Streams, CNo, SNo)
             end;
         {ok, {connect, N}} ->
-            %% create many connections
+            %% create N connections
             CC = lists:zf(
                    fun(NN) ->
                            case quicer:connect(
                                   "localhost", Port,
                                   [{alpn, ["sample"]},
-                                   {handshake_idle_timeout_ms, 3 * 45000},
-                                   {idle_timeout_ms, 0}
+                                   {keep_alive_interval_ms, ?INTERVAL},
+                                   {handshake_idle_timeout_ms, 3 * ?INTERVAL},
+                                   {idle_timeout_ms, 3 * ?INTERVAL}
                                   ], 10000) of
                                {ok, Conn} ->
                                    io:format("Connection # ~p~n", [NN]),
@@ -292,6 +263,7 @@ client(Conns, Streams, CNo, SNo) ->
             client(Conns, Streams, CNo, SNo);
 
         {ok, {stream, CNumber}} ->
+            %% Create a stream on connection CNumber
             {value, {Conn, CNumber}} = lists:keysearch(CNumber, 2, Conns),
             case quicer:start_stream(Conn, [{active, 30}]) of
                 {ok, Stm} ->
@@ -311,6 +283,7 @@ client(Conns, Streams, CNo, SNo) ->
                     client(Conns, Streams, CNo, SNo)
             end;
         {ok, {stream, CNumber, NN}} ->
+            %% Create NN  streams on connection CNumber
             {value, {Conn, CNumber}} = lists:keysearch(CNumber, 2, Conns),
             S2 = lists:map(
                    fun(N) ->
@@ -332,63 +305,35 @@ client(Conns, Streams, CNo, SNo) ->
             io:format("closing connection -> ",[]),
             Ret = quicer:close_connection(Conn),
             io:format("~p \n",[Ret]),
-            S2 = lists:filter(fun({_Stream, X, _Y}) ->
-                                      if X == CNumber -> false;
-                                         true -> true
-                                      end
-                              end, Streams),
-            client(lists:keydelete(CNumber, 2, Conns), S2, CNo, SNo);
+            client(lists:keydelete(CNumber, 2, Conns), Streams, CNo, SNo);
         {ok, {close_stream, SNumber}} ->
             {value, {Stm, _, SNumber}} = lists:keysearch(SNumber, 3, Streams),
             Ret = quicer:close_stream(Stm),
             io:format("~p~n", [Ret]),
             client(Conns, lists:keydelete(SNumber, 3, Streams), CNo, SNo);
         {ok, {ping, SNumber}} ->
+            %% Send ping on stream SNumber
             {value, {Stm, _, SNumber}} = lists:keysearch(SNumber, 3, Streams),
             Ret = send_ping(1, Stm),
             io:format("~p~n", [Ret]),
             client(Conns, Streams, CNo, SNo);
         {ok, {ping, SNumber, N}} ->
+            %% Send N pings on stream SNumber
             {value, {Stm, _ , SNumber}} = lists:keysearch(SNumber, 3, Streams),
             Ret = send_ping(N, Stm),
             io:format("~p~n", [Ret]),
             client(Conns, Streams, CNo, SNo);
 
         {ok, {die, SNumber}} ->
+            %% Force other end of stream to die without closing stream
             {value, {Stm, _, SNumber}} = lists:keysearch(SNumber, 3, Streams),
             Ret = quicer:send(Stm, <<"die">>),
             io:format("~p~n", [Ret]),
             client(Conns, Streams, CNo, SNo);
-        {ok, {turn, SNumber}} ->
-            %% Turn this connection around, getting streams from the
-            %% other end.
-            {value, {Stm, CNumber, SNumber}} =
-                lists:keysearch(SNumber, 3, Streams),
-            {ok, 4} = quicer:send(Stm, <<"turn">>),
-            quicer:close_stream(Stm),
-            {value, {Conn, _}} = lists:keysearch(CNumber, 2, Conns),
-
-            case accept_stream(Conn, [{active, false}], infinity) of
-                {ok, Stm2} ->
-                    io:format("accept stream -> ~p\n",[Stm2]),
-                    case server_negotiate(Stm2) of
-                        {ok, 1, 1} ->
-                            io:format("Enter recv_ping for ~p:~p\n",[1,1]),
-                            ok = quicer:setopt(Stm2, active, 20),
-                            RR = recv_ping(Stm2, 1, 1, 1),
-                            io:format("recv ping = ~p~n", [RR]);
-                        Err ->
-                            io:format("ERr ~p~n", [Err])
-                    end;
-                Err ->
-                    io:format("ERr2 ~p~n", [Err])
-            end,
-            S2 = lists:keydelete(SNumber, 3, Streams),
-            client(Conns, S2, CNo, SNo);
 
         {ok, print} ->
             io:format("Conns = ~p~n", [Conns]),
-            io:format("Streans = ~p~n", [Streams]),
+            io:format("Streams = ~p~n", [Streams]),
             client(Conns, Streams, CNo, SNo);
         {ok, flush} ->
             {C2, S2} = flush(Conns, Streams),
@@ -407,9 +352,9 @@ client_negotiate(Stm, CNo, SNo) ->
         {ok, 8} ->
             io:format("Sent negotiate data \n",[]),
             receive
-                {quic, <<"dataok">>, Stm, _,_,_} ->
+                {quic, <<"dataok">>, Stm, _} ->
                     ok;
-                {quic, closed, Stm} ->
+                {quic, stream_closed, Stm, _} ->
                     closed
             end;
         Err ->
@@ -429,43 +374,19 @@ server_negotiate(Stm) ->
 
 flush(Conns, Streams) ->
     receive
-        {quic, closed, Conn} ->
+        {quic, transport_shutdown, Conn, _} ->
             {value, {Conn, CNumber}} = lists:keysearch(Conn, 1, Conns),
             io:format("Connection ~p closed \n", [CNumber]),
             flush(lists:keydelete(Conn, 1, Conns), Streams);
-        {quic, closed, Stm, _} ->
+        {quic, stream_closed, Stm, _} ->
             {value, {Stm, _ , SNumber}} = lists:keysearch(Stm, 1, Streams),
             io:format("Stream ~p closed \n",[SNumber]),
             flush(Conns, lists:keydelete(Stm, 1, Streams));
         X ->
-            io:format("~p~n", [X]),
+            io:format("OTHER: ~p~n", [X]),
             flush(Conns, Streams)
     after 1 ->
             {Conns, Streams}
     end.
 
 
-
-default_stream_opts() ->
-  #{active => true}.
-
-accept_stream(Conn, Opts, Timeout) when is_list(Opts) ->
-  accept_stream(Conn, maps:from_list(Opts), Timeout);
-accept_stream(Conn, Opts, Timeout) when is_map(Opts) ->
-  % @todo make_ref
-  % @todo error handling
-  NewOpts = maps:merge(default_stream_opts(), Opts),
-  case quicer_nif:async_accept_stream(Conn, NewOpts) of
-    {ok, Conn} ->
-      receive
-        {quic, new_stream, Stream} ->
-          {ok, Stream};
-        {quic, closed, Conn} ->
-          {error, connection_closed}
-
-      after Timeout ->
-          {error, timeout}
-      end;
-    {error, _} = E ->
-      E
-  end.
